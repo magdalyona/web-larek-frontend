988108eccafeebc8912dcec994fe5ce425ddd4f4
Aleyna <erre_way@mail.ru>
2025-04-11 22:32:15 +0300

    1 часть с архитектурой приложения, чтобы сдать 8 спринт

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 import './scss/styles.scss';
+
+import { API_URL, CDN_URL } from './utils/constants';
+
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,5 +2,18 @@ export const API_URL = `${process.env.API_ORIGIN}/api/weblarek`;
 export const CDN_URL = `${process.env.API_ORIGIN}/content/weblarek`;
 
 export const settings = {
-
+    headers: {
+        authorization: `${process.env.API_TOKEN}`,
+        'Content-Type': 'application/json',
+    },
 };
+
+export const categories = new Map([
+
+    ["дополнительное", "additional"],
+    ["софт-скил", "soft"],
+    ["кнопка", "button"],
+    ["хард-скил", "hard"],
+    ["другое", "other"],
+
+]);
